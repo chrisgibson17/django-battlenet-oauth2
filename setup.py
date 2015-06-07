49ffc6ac0c164ff1d949adf73dc76b0365935c68
Chris Gibson <chrisgibson17@googlemail.com>
2015-06-07 03:09:24 +0100

    .

--- setup.py
+++ setup.py
@@ -2,33 +2,12 @@ from distutils.core import setup
 
 setup(name='django-battlenet',
     license='MIT',
-    version='0.1.1',
-    py_modules=[
-        'battlenet.constants',
-        'battlenet.oauth2',
-        'battlenet.community.wow.achievements',
-        'battlenet.community.wow.auctions',
-        'battlenet.community.wow.battlepets',
-        'battlenet.community.wow.challengemodes',
-        'battlenet.community.wow.characters',
-        'battlenet.community.wow.guilds',
-        'battlenet.community.wow.items',
-        'battlenet.community.wow.pvp',
-        'battlenet.community.wow.quests',
-        'battlenet.community.wow.realms',
-        'battlenet.community.wow.recipe',
-        'battlenet.community.wow.spells',
-        'battlenet.community.wow.data',
-        'battlenet.community.sc2',
-        'battlenet.community.d3'
-    ],
+    version='0.0.3',
+    py_modules=[''],
     packages=['battlenet', 'battlenet.oauth2', 'battlenet.community'],
-    install_requires=[
-        'requests_oauth2'
-    ],
-    description='API wrapper for Blizzards Community APIs & oAuth login',
+    description='API wrapper for Blizzards Community REST APIs & oAuth2 login',
     author='Chris Gibson',
     author_email='chris@chrisgibson.io',
     url='https://github.com/chrisgibson17/django-battlenet-oauth2',
-    download_url= 'https://github.com/chrisgibson17/django-battlenet-oauth2/archive/master.tar.gz',
+    download_url='https://github.com/chrisgibson17/django-battlenet-oauth2/archive/master.tar.gz',
 )
